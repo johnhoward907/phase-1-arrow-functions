7819b6cf4309561b111f66989cb2ca78b3f1f8d0
John <jjhhoward1738@gmail.com>
2025-03-19 09:55:51 +0300

    adds files

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(param1,param2) {
+    return 2000 / 100;
+  };
+  divide(2000,100)
+
+  const square= (num)=>{
+    return num*num;
+  }
+  const add = (para1,para2)=>{
+    return para1+para2
+  }
